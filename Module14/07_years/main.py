41aa64e5bbd930ce5dd5ed6135f0d0c3569474e5
pavel <gill1488@mail.ru>
2021-05-30 18:29:48 +0300

    task 7 is done

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,25 @@
 # TODO здесь писать код
+
+def count_year(year):
+    count, i = 0, year % 10
+
+    while year > 0:
+        if i == year % 10:
+            count += 1
+            i = year % 10
+
+        year //= 10
+
+    if count == 3:
+        return True
+
+    else:
+        return False
+
+
+year1 = int(input('Введите первый год: '))
+year2 = int(input('Введите второй год: '))
+
+for year in range(year1, (year2 + 1)):
+    if count_year(year):
+        print(year)
