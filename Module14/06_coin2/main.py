ada3d00539b7f816aab4bb8fa4534b9164580e15
pavel <gill1488@mail.ru>
2021-05-30 17:57:16 +0300

    task 6 is done

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+
+def in_circle(x, y, r):
+
+    if (x ** 2 + y ** 2) <= r ** 2:
+        return 'Монетка где-то рядом'
+
+    else:
+        return 'Монетки в области нет'
+
+
+print('Введите координаты монетка:')
+x1, y1 = float(input('X: ')), float(input('Y: '))
+r1 = float(input('Введите радиус: '))
+print(f'\n{in_circle(x1, y1, r1)}')
