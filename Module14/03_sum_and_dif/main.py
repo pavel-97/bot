920ae10125062382b690f247b6a424d854825633
pavel <gill1488@mail.ru>
2021-05-30 16:25:58 +0300

    task 3 is done

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,22 @@
 # TODO здесь писать код
+
+def summ(n):
+    summ_n = 0
+    while n != 0:
+        summ_n += n % 10
+        n //= 10
+    return summ_n
+
+
+def count(n):
+    count_n = 0
+    while n != 0:
+        n //= 10
+        count_n += 1
+    return count_n
+
+
+n = int(input('Введите число:'))
+print('Сумма цифр:', summ(n))
+print('Кол-во цифр в числе:', count(n))
+print('Разность суммы и кол-ва цифр:', summ(n) - count(n))
