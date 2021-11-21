a4acc8d0566e46302d0153d9457ebd8a3daa73ee
pavel <gill1488@mail.ru>
2021-11-21 19:48:26 +0300

    diploma 21.11.2021

--- diploma/history.py
+++ diploma/history.py
@@ -1,17 +1,18 @@
-from models import History
+from models import History, db
 
 
-def history(message):
+def history(message) -> str:
     """Функция открывает содинение с БД,
     Извлекает данные и отправляет их
     пользователю."""
     results = []
-    list_history = History.select().where(History.user_id == message.chat.id)
-    for row in list_history[None if len(list_history) <= 5 else -5:]:
-        row_i = 'Команда: {}; Дата: {}; Отели: {}.\n'.format(
-            row.command,
-            row.date,
-            row.hotels
-        )
-        results.append(row_i)
+    with db:
+        list_history = History.select().where(History.user_id == message.chat.id)
+        for row in list_history[None if len(list_history) <= 5 else -5:]:
+            row_i = 'Команда: {}; Дата: {}; Отели: {}.\n'.format(
+                row.command,
+                row.date,
+                row.hotels
+            )
+            results.append(row_i)
     return '\n'.join(results) if results else 'Ваша история пуста'
